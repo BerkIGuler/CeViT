24deb527996a1282a1554b0bcd72a69325d057a7
BerkIGuler <bierkay1@icloud.com>
2024-05-08 13:23:00 -0700

    Data Split Added Test Set Evaluation

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from preprocess import PatchEmbedding, InversePatchEmbedding
 from dataloader import MatDataset
 from torch.utils.data import DataLoader
@@ -6,8 +7,8 @@ from token_module import TokenModule
 from model import Encoder
 
 
-batch_size = 64
-epoch = 10
+batch_size = 32
+epoch = 40
 model_dim = 128  # transformer linear projection dim
 n_head = 4
 patch_dim = 40  # patch embedding dim
@@ -15,9 +16,20 @@ input_dim = 46
 dropout = 0.1
 token_embedding_dim = 168
 device = "cuda:0"
-data_dir = "data"
-dataset = MatDataset(data_dir)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+train_data_dir = "train_data"
+test_data_dir = "test_data"
+val_data_dir = "val_data"
+
+train_dataset = MatDataset(train_data_dir)
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+val_dataset = MatDataset(val_data_dir)
+val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+
+test_dataset = MatDataset(test_data_dir)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
 patcher = PatchEmbedding().to(device)
 inverse_patcher = InversePatchEmbedding().to(device)
 tokenizer = TokenModule(input_size=1, embedding_dim=token_embedding_dim).to(device)
@@ -32,8 +44,9 @@ encoder = Encoder(
 optimizer = torch.optim.Adam(encoder.parameters(), lr=1e-3)
 
 for ep in range(epoch):
-    epoch_loss = 0.0
-    for batch in dataloader:
+    train_loss = 0.0
+    encoder.train()
+    for batch in train_dataloader:
         optimizer.zero_grad()
         ls_channel, ideal_channel, meta_data = batch
         ls_channel, ideal_channel = ls_channel.to(device), ideal_channel.to(device)
@@ -53,7 +66,29 @@ for ep in range(epoch):
 
         optimizer.step()
 
-        epoch_loss += output.item() * batch_size  # Accumulate batch loss
+        train_loss += output.item() * batch_size  # Accumulate batch loss
 
-    epoch_loss /= len(dataset)  # Calculate average epoch loss
-    print(f"Epoch [{ep+1}/{epoch}], Loss: {epoch_loss:.4f}")
+    train_loss /= len(train_dataset)  # Calculate average epoch loss
+    print(f"Train [{ep+1}/{epoch}], Loss: {20 * np.log10(train_loss):.4f}")
+
+    val_loss = 0.0
+    encoder.eval()
+    for batch in val_dataloader:
+        ls_channel, ideal_channel, meta_data = batch
+        ls_channel, ideal_channel = ls_channel.to(device), ideal_channel.to(device)
+        file_no, SNR, delay_spread, max_dop_shift, ch_type = meta_data
+        SNR, delay_spread, max_dop_shift = SNR.to(device), delay_spread.to(device), max_dop_shift.to(device)
+
+        ls_channel = patcher(ls_channel)
+        token_encodings = tokenizer(SNR, delay_spread, max_dop_shift)
+        model_input = torch.cat(tensors=(ls_channel, token_encodings), dim=2)
+
+        encoder_output = encoder(model_input)
+        estimated_channel = inverse_patcher(encoder_output)
+
+        loss = torch.nn.MSELoss()
+        output = loss(estimated_channel, ideal_channel)
+        val_loss += output.item() * batch_size  # Accumulate batch loss
+
+    val_loss /= len(val_dataset)  # Calculate average epoch loss
+    print(f"Val [{ep+1}/{epoch}], Loss: {20 * np.log10(val_loss):.4f}")
