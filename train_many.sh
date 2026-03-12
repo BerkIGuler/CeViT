8ba0a78d6f6fc2851a4f9885f455bce1afdf8b86
Berkay Guler <bierkay1@icluod.com>
2026-03-11 17:26:03 -0700

    Enhance CeViT model training by adjusting learning rate schedule, refining data augmentation techniques, and optimizing loss function calculations. Update relevant configuration files to reflect these changes.

---
+++ train_many.sh
@@ -0,0 +1,17 @@
+python3 train.py configs/train_tdla_2.yaml --device cuda:1
+python3 train.py configs/train_tdlb_2.yaml --device cuda:1
+python3 train.py configs/train_tdlc_2.yaml --device cuda:1
+python3 train.py configs/train_tdld_2.yaml --device cuda:1
+python3 train.py configs/train_tdle_2.yaml --device cuda:1
+
+python3 train.py configs/train_tdla_23.yaml --device cuda:1
+python3 train.py configs/train_tdlb_23.yaml --device cuda:1
+python3 train.py configs/train_tdlc_23.yaml --device cuda:1
+python3 train.py configs/train_tdld_23.yaml --device cuda:1
+python3 train.py configs/train_tdle_23.yaml --device cuda:1
+
+python3 train.py configs/train_tdla_2711.yaml --device cuda:1
+python3 train.py configs/train_tdlb_2711.yaml --device cuda:1
+python3 train.py configs/train_tdlc_2711.yaml --device cuda:1
+python3 train.py configs/train_tdld_2711.yaml --device cuda:1
+python3 train.py configs/train_tdle_2711.yaml --device cuda:1
